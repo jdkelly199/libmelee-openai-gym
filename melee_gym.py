74191b72d49437bf1236f07c90c28c660fa7686f
Ian MacFarlane <ian@macfarlanehome.com>
2021-12-06 20:27:06 -0800

    Add files via upload

--- melee_gym.py
+++ melee_gym.py
@@ -10,24 +10,24 @@ class MeleeEnv(gym.GoalEnv):
   """Custom Environment that follows gym interface"""
   metadata = {'render.modes': ['human']}
 
-  def __init__(self, ai=0, cpu=0, training_iterations=1000000):
+  def __init__(self, ai=0, cpu=0, enemy_level=1, training_iterations=1000000):
     super(MeleeEnv, self).__init__()
     # Define action and observation space
     # They must be gym.spaces objects
     # Example when using discrete actions:
-    self.action_space = spaces.Discrete(26)
+    self.action_space = spaces.Discrete(21)
     # Example for using image as input:
     self.observation_space = spaces.Dict(
         {
-            "observation": spaces.Box(np.array([0, 0, -1000, -1000, 0, 0, 0, 0, 0, 0, 0, 0, -1000, -1000, 0, 0, 0, 0, 0, 0]), #player 1(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable), player 2(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable)
-                        np.array([4, 1000, 1000, 1000, 1, 1, 1, 500, 10, 1, 4, 1000, 1000, 1000, 1, 1, 1, 500, 10, 1])), #player 1(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable), #player 2(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable)
-            "achieved_goal": spaces.Box(np.array([0, 0, -1000, -1000, 0, 0, 0, 0, 0, 0, 0, 0, -1000, -1000, 0, 0, 0, 0, 0, 0]), #player 1(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable), player 2(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable)
-                        np.array([4, 1000, 1000, 1000, 1, 1, 1, 500, 10, 1, 4, 1000, 1000, 1000, 1, 1, 1, 500, 10, 1])),
-            "desired_goal": spaces.Box(np.array([0, 0, -1000, -1000, 0, 0, 0, 0, 0, 0, 0, 0, -1000, -1000, 0, 0, 0, 0, 0, 0]), #player 1(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable), player 2(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable)
-                        np.array([4, 1000, 1000, 1000, 1, 1, 1, 500, 10, 1, 4, 1000, 1000, 1000, 1, 1, 1, 500, 10, 1])),
+            "observation": spaces.Box(np.array([0, 0, -1000, -1000, 0, 0, 0, 0, 0, 0, 0, 0, -1000, -1000, 0, 0, 0, 0, 0, 0, -1]), #player 1(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable), player 2(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable)
+                        np.array([4, 1000, 1000, 1000, 1, 1, 1, 500, 10, 1, 4, 1000, 1000, 1000, 1, 1, 1, 500, 10, 1, 1])), #player 1(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable), #player 2(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable)
+            "achieved_goal": spaces.Box(np.array([0, 0, -1000, -1000, 0, 0, 0, 0, 0, 0, 0, 0, -1000, -1000, 0, 0, 0, 0, 0, 0, -1]), #player 1(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable), player 2(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable)
+                        np.array([4, 1000, 1000, 1000, 1, 1, 1, 500, 10, 1, 4, 1000, 1000, 1000, 1, 1, 1, 500, 10, 1, 1])),
+            "desired_goal": spaces.Box(np.array([0, 0, -1000, -1000, 0, 0, 0, 0, 0, 0, 0, 0, -1000, -1000, 0, 0, 0, 0, 0, 0, -1]), #player 1(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable), player 2(stock, percent, x, y, facing, on_ground, off_stage, action, jumps_left, invulnerable)
+                        np.array([4, 1000, 1000, 1000, 1, 1, 1, 500, 10, 1, 4, 1000, 1000, 1000, 1, 1, 1, 500, 10, 1, 1])),
         })
 
-    self.melee_characters = [melee.enums.Character.BOWSER, melee.enums.Character.CPTFALCON, melee.enums.Character.DK, melee.enums.Character.DOC, melee.enums.Character.FALCO, melee.enums.Character.FOX, melee.enums.Character.GAMEANDWATCH, melee.enums.Character.GANONDORF, melee.enums.Character.JIGGLYPUFF, melee.enums.Character.LINK, melee.enums.Character.LUIGI, melee.enums.Character.MARIO, melee.enums.Character.MARTH, melee.enums.Character.MEWTWO, melee.enums.Character.NESS, melee.enums.Character.PEACH, melee.enums.Character.PICHU, melee.enums.Character.PIKACHU, melee.enums.Character.ROY, melee.enums.Character.SAMUS, melee.enums.Character.YLINK, melee.enums.Character.YOSHI, melee.enums.Character.ZELDA]
+    self.melee_characters = [melee.enums.Character.CPTFALCON]
 
     #download Slippi (https://slippi.gg/) and insert path here to dolphin (might be in your temp) here (I can send you a link to the iso when you get here so you do not need to download it elsewhere)
     self.console = melee.Console(path="C:/Users/Ian/AppData/Roaming/Slippi Launcher/netplay")#"C:/Users/Ian/OneDrive/Desktop/CS238/Slippi Dolphin")
@@ -46,10 +46,10 @@ class MeleeEnv(gym.GoalEnv):
         self.cpu_controller = melee.Controller(console=self.console, port=2, type=melee.ControllerType.GCN_ADAPTER)
 
     self.dolphin = False
-    self.cpu_level = 1
+    self.cpu_level = enemy_level
     self.reward = -1
     self.accomplished = False
-    self.desired_goal = np.array([4, 0, -1000, -1000, 0, 0, 0, 0, 0, 0, 3, 0, -1000, -1000, 0, 0, 0, 0, 0, 0])
+    self.desired_goal = np.array([4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, -1000, -1000, 0, 0, 0, 0, 0, 0, 0])
 
     self.cpu_char = self._get_random_char()
 
@@ -59,10 +59,17 @@ class MeleeEnv(gym.GoalEnv):
 
   def compute_reward(self, achieved_goal, desired_goal, info={}):
       reward = -1
-      if achieved_goal[10] == desired_goal[10] and achieved_goal[0] == desired_goal[0]:
-          self.accomplished = True
-          reward = 0
+      if achieved_goal[10] == desired_goal[10]:
+          if achieved_goal[10] == self.desired_goal[10]:
+              self.accomplished = True
+          reward = 1
       return reward
+
+  def isAccomplished(self):
+      return self.accomplished
+
+  def getStockAdvantage(self):
+      return self.obs['observation'][0] - self.obs['observation'][10]
 
   def _get_random_char(self):
       return self.melee_characters[random.randint(0, len(self.melee_characters) - 1)]
@@ -143,7 +150,7 @@ class MeleeEnv(gym.GoalEnv):
             melee.menuhelper.MenuHelper.menu_helper_simple(self.gamestate,
                                                            self.controller,
                                                            melee.enums.Character.KIRBY,
-                                                           melee.enums.Stage.RANDOM_STAGE,
+                                                           melee.enums.Stage.FINAL_DESTINATION,
                                                            "",
                                                            autostart=False,
                                                            swag=True)
@@ -161,7 +168,7 @@ class MeleeEnv(gym.GoalEnv):
         melee.menuhelper.MenuHelper.menu_helper_simple(self.gamestate,
                                                        self.cpu_controller,
                                                        self.cpu_char,
-                                                       melee.enums.Stage.RANDOM_STAGE,
+                                                       melee.enums.Stage.FINAL_DESTINATION,
                                                        "",
                                                        cpu_level=self.cpu_level,
                                                        autostart=True,
@@ -173,6 +180,12 @@ class MeleeEnv(gym.GoalEnv):
     return self._next_observation()
 
   def _next_observation(self):
+
+      recovery_obs = 0
+      if self.gamestate.players[1].position.x >= 90 or (self.gamestate.players[1].position.x > 0 and self.gamestate.players[1].position.y < 0):
+          recovery_obs = 1
+      if self.gamestate.players[1].position.x <= -90 or (self.gamestate.players[1].position.x < 0 and self.gamestate.players[1].position.y < 0):
+          recovery_obs = -1
 
       obs = np.array([int(self.gamestate.players[1].stock), #0
              int(self.gamestate.players[1].percent),        #1
@@ -193,7 +206,8 @@ class MeleeEnv(gym.GoalEnv):
              int(self.gamestate.players[2].off_stage),      #16
              int(self.gamestate.players[2].action._value_), #17
              int(self.gamestate.players[2].jumps_left),     #18
-             int(self.gamestate.players[2].invulnerable)])  #19
+             int(self.gamestate.players[2].invulnerable),   #19
+             int(recovery_obs)])  #20
 
       return OrderedDict([
             ('observation', obs),
--- train_and_play.py
+++ train_and_play.py
@@ -3,45 +3,51 @@ from stable_baselines.common.vec_env import DummyVecEnv, VecNormalize
 from stable_baselines.common.callbacks import CallbackList, CheckpointCallback, EveryNTimesteps
 from stable_baselines import PPO2, HER, DQN
 import csv_callback
+import time
 
-env = DummyVecEnv([lambda: MeleeEnv(cpu=False)])
+env = DummyVecEnv([lambda: MeleeEnv(cpu=False, enemy_level=(2))])
 #env = VecNormalize(env, norm_obs=True, norm_reward=True)
 
 nsg = 4
 logs_path = "C:/Users/Ian/OneDrive/Desktop/CS238/libmelee/logs/"
-
 #model = PPO2.load('models/PPO2_pretrain_rl_long', env)
 model = HER('MlpPolicy',env,DQN,n_sampled_goal=nsg, \
             goal_selection_strategy='future', verbose=1, \
             buffer_size=int(1e8), learning_rate=1e-3, gamma=0.95, \
             batch_size=256, policy_kwargs=dict(layers=[256,256,256]))
 
+model.save(logs_path + "her_v4")
+env.close()
+time.sleep(1)
 win_ct = 0
+stock_adv = []
 
 for i in range(100):
     print(f'EPOCH: {i}')
-    env = DummyVecEnv([lambda: MeleeEnv(cpu=False)])
-    model = HER.load(logs_path + "her_v1", env=env)
+    env = MeleeEnv(cpu=False, enemy_level=((i+200)//100))
+    model = HER.load(logs_path + "her_v4", env=env)
     try:
-        model.learn(total_timesteps=8000)
+        model.learn(total_timesteps=5000)
         print("Success. Saving...")
-        model.save(logs_path + "her_v1")
-        if env.accomplished:
-            win_ct += 1
+        model.save(logs_path + "her_v3")
     except:
         print("Timeout Occurred.")
+    if env.isAccomplished():
+        win_ct += 1
+    stock_adv.append(env.getStockAdvantage())
     print(f'Running Average: {win_ct / (i+1)}')
+    print(stock_adv)
     env.close()
     print("Done")
-    input("Press Enter to continue...")
-
+    time.sleep(1)
+    if (i%20 == 0):
+        input("Press enter to continue...")
 #env.save(logs_path + "vec_normalize.pkl")
 
-
-obs = env.reset()
-
-for i in range(500):
+env = DummyVecEnv([lambda: MeleeEnv(cpu=False, enemy_level=((200)//100))])
+for i in range(1000):
     print("testing:" + str(i))
     action, _states = model.predict(obs)
     obs, rewards, done, info = env.step(action)
     env.render()
+env.close()
